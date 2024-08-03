9a10119ad88121bdf25f2e01a8ff26985f686af8
mdnazmul42726 <nazmulofficial@outlook.com>
2024-08-03 19:02:33 +0600

    empty response handled successfully

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,20 +1,45 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Footer from "../components/Global/nav/Footer";
 import PrdctCard from "../components/home/PrdctCard";
 import { useGetProductsQuery } from "../redux/features/products/product.api";
+import noItem from '../../public/no-product-8316266-6632286.webp';
 
 const Products = () => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const category = searchParams.get('category');
-
     const { data, isError, isLoading } = useGetProductsQuery({ category });
 
     if (isLoading) return <div className="">Loading...</div>;
     if (isError) return <div className="">Something Wrong!</div>;
 
+    if (!data.data.length && category) return (
+        <div className="h-screen w-full border flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
+                <img className="size-36" src={noItem} alt="" />
+                <p className="text-xl">No Item Available In <span className="text-rose-600">{category?.toUpperCase()}</span> Category!</p>
+                <div className="flex gap-3 mt-3">
+                    <Link to={'/dashboard/new'}>  <button className="py-1.5 px-3 bg-rose-600 text-white rounded-sm">Add One</button></Link>
+                    <Link to={'/products'} >  <button className="py-1.5 px-3 bg-white text-rose-600 border rounded-sm">Load All</button></Link>
+                </div>
+            </div>
+        </div>
+    )
+
+    if (!data.data.length) return (
+        <div className="h-screen w-full border flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
+                <img className="size-36" src={noItem} alt="" />
+                <p className="text-xl">No Item Available</p>
+                <div className="flex gap-3 mt-3">
+                    <Link to={'/dashboard/new'}>  <button className="py-1.5 px-3 bg-rose-600 text-white rounded-sm">Add One</button></Link>
+                </div>
+            </div>
+        </div>
+    )
+
     return (
-        <div className="">
+        <div className="h-screen">
 
             <div className=" p-4 md:p-10 lg:p-14">
 
