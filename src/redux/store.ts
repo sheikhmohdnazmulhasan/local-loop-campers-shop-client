c38c8db4471069f8016c39c626984b418b30db2d
mdnazmul42726 <nazmulofficial@outlook.com>
2024-08-03 10:25:49 +0600

    DefaultMiddleware added on store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { baseApi } from './api/baseApi'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [baseApi.reducerPath]: baseApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 
