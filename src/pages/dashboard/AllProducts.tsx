3cb723808fc89e38c7c05fb04d56a5457bcbd15b
mdnazmul42726 <nazmulofficial@outlook.com>
2024-08-03 12:16:29 +0600

    successfullty fetched single products by rtk

--- src/pages/dashboard/AllProducts.tsx
+++ src/pages/dashboard/AllProducts.tsx
@@ -1,5 +1,11 @@
+import { useGetProductsQuery } from "../../redux/features/products/product.api";
 
 const AllProducts = () => {
+
+    const { data, isError, isLoading } = useGetProductsQuery(undefined);
+
+    console.log({ data, isError, isLoading });
+
     return (
         <div>
             hello
--- src/redux/features/products/product.api.ts
+++ src/redux/features/products/product.api.ts
@@ -2,6 +2,22 @@ import { baseApi } from "../../api/baseApi";
 
 const productApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: (query) => {
+                const params = new URLSearchParams();
+                let url = '/products'
+
+                if (query?.category) params.append('category', query.category as string);
+                if (query?.id) url = `/products/${query.id}`;
+
+                return {
+                    url,
+                    method: 'GET',
+                    params
+                }
+            }
+        }),
+
         createProduct: builder.mutation({
             query: (payload) => ({
                 url: '/products/new',
@@ -12,4 +28,4 @@ const productApi = baseApi.injectEndpoints({
     })
 })
 
-export const { useCreateProductMutation } = productApi
+export const { useGetProductsQuery, useCreateProductMutation } = productApi
